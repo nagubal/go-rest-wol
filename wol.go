8b152df9c8cde996ef82f69801dc5e7cf3e47310
nagubal <45527+nagubal@users.noreply.github.com>
2019-06-10 15:19:59 +0100

    add dependcy : github.com/sabhiram/go-wol

--- wol.go
+++ wol.go
@@ -8,61 +8,8 @@ import (
 	"errors"
 	"log"
 	"net"
-	"regexp"
+"github.com/sabhiram/go-wol"
 )
-
-// Define globals for the MacAddress parsing
-var (
-	delims = ":-"
-	re_MAC = regexp.MustCompile(`^([0-9a-fA-F]{2}[` + delims + `]){5}([0-9a-fA-F]{2})$`)
-)
-
-type MACAddress [6]byte
-
-// A MagicPacket is constituted of 6 bytes of 0xFF followed by
-// 16 groups of the destination MAC address.
-type MagicPacket struct {
-	header  [6]byte
-	payload [16]MACAddress
-}
-
-// This function accepts a MAC Address string, and returns a pointer to
-// a MagicPacket object. A Magic Packet is a broadcast frame which
-// contains 6 bytes of 0xFF followed by 16 repetitions of a given mac address.
-func NewMagicPacket(mac string) (*MagicPacket, error) {
-	var packet MagicPacket
-	var macAddr MACAddress
-
-	// We only support 6 byte MAC addresses since it is much harder to use
-	// the binary.Write(...) interface when the size of the MagicPacket is
-	// dynamic.
-	if !re_MAC.MatchString(mac) {
-		return nil, errors.New("MAC address " + mac + " is not valid.")
-	}
-
-	hwAddr, err := net.ParseMAC(mac)
-	if err != nil {
-		return nil, err
-	}
-
-	// Copy bytes from the returned HardwareAddr -> a fixed size
-	// MACAddress
-	for idx := range macAddr {
-		macAddr[idx] = hwAddr[idx]
-	}
-
-	// Setup the header which is 6 repetitions of 0xFF
-	for idx := range packet.header {
-		packet.header[idx] = 0xFF
-	}
-
-	// Setup the payload which is 16 repetitions of the MAC addr
-	for idx := range packet.payload {
-		packet.payload[idx] = macAddr
-	}
-
-	return &packet, nil
-}
 
 // This function gets the address associated with an interface
 func GetIpFromInterface(iface string) (*net.UDPAddr, error) {
@@ -97,7 +44,7 @@ func GetIpFromInterface(iface string) (*net.UDPAddr, error) {
 // receives an iface to broadcast on. An iface of "" implies a nil net.UDPAddr
 func SendMagicPacket(macAddr, bcastAddr, iface string) error {
 	// Construct a MagicPacket for the given MAC Address
-	magicPacket, err := NewMagicPacket(macAddr)
+	magicPacket, err := wol.New(macAddr)
 	if err != nil {
 		return err
 	}
